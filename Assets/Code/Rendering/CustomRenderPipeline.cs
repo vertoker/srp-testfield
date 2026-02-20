4372cc33d289edf4d577ac9b7adc7f9e18aee03d
vertoker <vertoker@gmail.com>
2026-02-20 12:49:36 +0500

    update srp code location

---
+++ Assets/Code/Rendering/CustomRenderPipeline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Code.Rendering
+{
+    public class CustomRenderPipeline : RenderPipeline
+    {
+        private readonly CustomRenderPipelineAsset _asset;
+
+        public CustomRenderPipeline(CustomRenderPipelineAsset asset)
+        {
+            _asset = asset;
+        }
+
+        protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
+        {
+            // 1. Clear frame
+            
+            // 2. Culling objects
+            
+            // 3. Drawing objects
+            
+        }
+    }
+}
---
+++ Assets/Code/Rendering/CustomRenderPipelineAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Code.Rendering
+{
+    [CreateAssetMenu(fileName = nameof(CustomRenderPipelineAsset), 
+        menuName = "Configs/Rendering/" + nameof(CustomRenderPipelineAsset))]
+    public class CustomRenderPipelineAsset : RenderPipelineAsset<CustomRenderPipeline>
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return new CustomRenderPipeline(this);
+        }
+    }
+}
